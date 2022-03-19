60dcf28de4d4760eee4d9ed2f02f3aedc066993c
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2022-03-19 16:10:36 -0700

    playfair beginning

---
+++ ciberseguridad/java/playfair/src/Main.java
@@ -0,0 +1,23 @@
+public class Main{
+    public static String message;
+    public static char[] newKey;
+    public static char[] defKey = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    public static void makeKey(String val) {
+        for (int i = 0; i < val.length() - 1; i++) {
+            if(newKey.){
+                newKey[i] = val.charAt(i);
+            }
+        }
+        for (int i = 0; i < (25 - val.length()); i++) {
+            
+        }
+    }
+
+    public static void cipher() {
+
+    }
+    public static void main(String[] args) {
+        cipher();
+    }
+}
